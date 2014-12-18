9fe84d54432675af6ef7bf1c017fcf361dfb42fe
Neurocinetics <0901653@student.hr.nl>
2014-12-18 15:44:34 +0100

    Uploaded main.c code(Multithreaded), code builds not tested yet.

--- code/rijden/main.c
+++ code/rijden/main.c
@@ -9,19 +9,85 @@
 Function drive_goto( , ) is used to finely turn the bot around.
 */
 
-int main(){
-//Initialization variables and settings
-  double completeturn = 51.12;
-  double rightw  = -25.56;
-  double leftw = 25.56;
-  drive_setRampStep(10);
-  int turncounter = 0;
- 
-//main loop, alter at your own risk :3
- while(1){
-   drive_ramp(64, 64);
+
+//Initialization global variables and settings
+static volatile double completeturn = 51.12;
+static volatile double rightw  = -25.56;
+static volatile double leftw = 25.56;
+static volatile int turncounter = 0;
+
+//Setup pin variables for oversight
+static int pinU = 8;
+static int pinIRL = 9;
+static int pinIRR = 10;
+static int pinIRin0 = 2;
+static int pinIRin1 = 3; 
+
+//More memory than needed is assigned!
+//The CPU should have enough registers and other memory sources for this. 
+unsigned int cogStack = 32;
+unsigned int ExtraStackCog1 = 24;
+unsigned int ExtraStackCog2 = 32;
+static volatile int distance;
+static volatile int irLstate = 0;
+static volatile int irRstate = 0;
+
+
+void printDistance(void){
+  while(1)
+  {   
+    print("distance = " + distance);
+    pause(500);          
+  }
+}
+
+  void irSensorL(void){
+    while(1){
+      //freqout (int pin, int msTime, int frequency)
+      freqout(pinIRL, 1, 38000);
+      irLstate = input(pinIRin0);
+    }
+  }
+
+  void irSensorR(void){
+    while(1){
+      //freqout (int pin, int msTime, int frequency)
+      freqout(pinIRR, 1, 38000);
+      irRstate = input(pinIRin1);
+    }
+  }
+
+void irPrint(void){
+  while(1){
+    print("L = " + irLstate);
+  }
+}  
+
+void uSensor(void){
+  while(1){
     
-//check the turn counter and reverse direction
+    drive_ramp(64, 64);
+    distance = ping_cm(pinU);
+
+    while(ping_cm(pinU) < 10){
+
+      if(ping_cm(pinU) < 5){
+
+        while(ping_cm(pinU) < 5){
+
+        drive_ramp(-32, -32);
+        pause(500);
+        }
+      }
+      drive_ramp(0, 0);
+      drive_goto(leftw, rightw);
+      turncounter++;
+    }
+  } 
+}
+
+void turnCount(void){
+  //check the turn counter and reverse direction
    if(turncounter == 2){
      turncounter = 0;
      if(leftw == -25.56){
@@ -32,14 +98,13 @@ int main(){
         abs(rightw);
      }       
    }
-    
-   while(ping_cm(8) > 8)pause(5);
-   drive_ramp(0, 0);
-   drive_goto(leftw, rightw);
-   turncounter++;
-   
-   while(ping_cm(8) < 5){
-    drive_ramp(-32, -32);
-   }      
- }   
+}
+
+//main loop, alter at your own risk :3
+int main(){
+  cogstart(&uSensor, NULL, cogStack+ExtraStackCog1, sizeof(cogStack));
+  cogstart(&irSensorL, NULL, cogStack, sizeof(cogStack));
+  cogstart(&irSensorR, NULL, cogStack, sizeof(cogStack));
+  cogstart(&printDistance, NULL, cogStack, sizeof(cogStack));
+  cogstart(&turnCount, NULL, cogStack+ExtraStackCog2, sizeof(cogStack));
 }  
